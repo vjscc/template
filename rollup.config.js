bc3391c11b314f304c6498a4851dcc1f475a1706
Gu-Miao <1450206741@qq.com>
2021-09-29 22:38:22 +0800

    Add typescript support.

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import path from 'path'
+import pkg from './package.json'
+
+import clear from 'rollup-plugin-clear'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import ts from 'rollup-plugin-ts'
+import { terser } from 'rollup-plugin-terser'
+
+/** @type import('rollup').RollupOptions */
+const config = {
+  input: path.join(__dirname, 'src/index.ts'),
+  output: [
+    {
+      name: pkg.global,
+      file: pkg.main,
+      format: 'umd',
+      exports: 'default'
+    },
+    {
+      name: pkg.global,
+      file: pkg.browser,
+      format: 'iife',
+      exports: 'default',
+      plugins: [terser()],
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      exports: 'default'
+    }
+  ],
+
+  plugins: [
+    clear({
+      targets: ['dist']
+    }),
+    resolve(),
+    commonjs(),
+    ts({
+      transpiler: 'babel'
+    })
+  ]
+}
+
+export default config
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import sum from './utils/sum'
+
+interface IModal {
+  container: HTMLElement
+  body: string
+  index: number
+}
+
+class Modal implements IModal {
+  container: HTMLElement
+  body: string
+  index: number
+  constructor(container: HTMLElement, body: string, indexs: number[]) {
+    this.container = container
+    this.body = body
+    this.index = sum(...indexs)
+  }
+
+  static create(container: HTMLElement, body: string, indexs: number[]) {
+    return new Modal(container, body, indexs)
+  }
+
+  setBody = (body: string) => {
+    this.body = body
+    return this
+  }
+}
+
+export default Modal
