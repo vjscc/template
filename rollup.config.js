84c998de1dff8bae8a6339b1c542fef1312b2f47
Gu-Miao <1450206741@qq.com>
2022-03-29 15:55:02 +0800

    Update build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,62 +2,99 @@ import path from 'path'
 import pkg from './package.json'
 
 import clear from 'rollup-plugin-clear'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import less from 'rollup-plugin-less'
 import ts from 'rollup-plugin-ts'
 import { terser } from 'rollup-plugin-terser'
 import LessPluginCleanCSS from 'less-plugin-clean-css'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const input = path.join(__dirname, 'src/index.ts')
 
-/** @type {import('rollup').RollupOptions} */
-const config = {
-  input: path.join(__dirname, 'src/index.ts'),
-  output: [
-    {
-      name: pkg.global,
-      file: pkg.main,
-      format: 'umd',
-      exports: 'auto'
-    },
-    {
-      name: pkg.global,
-      file: pkg.browser,
-      format: 'umd',
-      exports: 'auto',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'esm',
-      exports: 'auto'
-    }
-  ],
-  plugins: [
+function getPlugins(lessConfig = {}, declaration) {
+  return [
     clear({
       targets: ['dist']
     }),
     resolve(),
     commonjs(),
-    less({
-      insert: !isProduction,
-      output: pkg.style,
-      option: {
-        plugins: isProduction
-          ? [
-              new LessPluginCleanCSS({
-                advanced: true
-              })
-            ]
-          : undefined
-      }
-    }),
+    less({ output: false, ...lessConfig }),
     ts({
-      transpiler: 'swc'
+      transpiler: 'swc',
+      tsconfig: {
+        declaration
+      }
     })
   ]
 }
+
+/**
+ * @typedef RollupOptions
+ * @type {import('rollup').RollupOptions}
+ */
+
+/**
+ * @returns {RollupOptions}
+ */
+function createDevConfig() {
+  return {
+    input,
+    output: {
+      name: pkg.global,
+      file: pkg.main,
+      format: 'umd',
+      exports: 'auto',
+      sourcemap: true
+    },
+    plugins: getPlugins({ insert: true }, true)
+  }
+}
+
+/**
+ * @returns {RollupOptions[]}
+ */
+function createProdConfig() {
+  return [
+    {
+      input,
+      output: {
+        name: pkg.global,
+        file: pkg.main,
+        format: 'umd',
+        exports: 'auto',
+        sourcemap: true
+      },
+      plugins: getPlugins({ output: pkg.css }, true)
+    },
+    {
+      input,
+      output: {
+        name: pkg.global,
+        file: pkg.browser,
+        format: 'umd',
+        exports: 'auto',
+        sourcemap: true,
+        plugins: [terser()]
+      },
+      plugins: getPlugins({
+        output: pkg.mincss,
+        plugins: [new LessPluginCleanCSS({ advanced: true })]
+      })
+    },
+    {
+      input,
+      output: {
+        file: pkg.module,
+        format: 'esm',
+        exports: 'auto',
+        sourcemap: true
+      },
+      plugins: getPlugins()
+    }
+  ]
+}
+
+const config = isProduction ? createProdConfig() : createDevConfig()
 
 export default config
--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export default class VjsccXX {
-  el: HTMLElement
-  text: string
-  show: boolean
-  hide(): void
-  tex(): void
-  constructor(el: HTMLElement, text: string, show?: boolean)
-}
